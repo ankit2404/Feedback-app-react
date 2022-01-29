cb64b62ed100e0e30fd4a79c77ecfb52c0b670fc
Ankit <mittalankit2404@gmail.com>
2022-01-29 20:46:19 +0530

    Second update

--- src/components/FeedbackList.jsx
+++ src/components/FeedbackList.jsx
@@ -2,34 +2,39 @@ import React from "react";
 import { motion, AnimatePresence } from "framer-motion";
 import Feedbackitem from "./Feedbackitem";
 import { useContext } from "react";
+import Spinner from "./shared/Spinner";
 import FeedbackContext from "../context/FeedbackContext";
 function FeedbackList({ handleDelete }) {
-  const { feedback } = useContext(FeedbackContext);
-  if (!feedback || feedback.length === 0) {
+  const { feedback, isLoading } = useContext(FeedbackContext);
+  if (!isLoading && (!feedback || feedback.length === 0)) {
     return "No Feedback Yet";
   }
-  return (
-    // <div className="feedback-list">
-    //   {feedback.map((item) => (
-    //     <Feedbackitem key={item.id} item={item} handleDelete={handleDelete} />
-    //   ))}
-    // </div>
 
-    <div className="feedback-list">
-      <AnimatePresence>
-        {feedback.map((item) => (
-          <motion.div
-            key={item.id}
-            initial={{ opacity: 0 }}
-            animate={{ opacity: 1 }}
-            exit={{ opacity: 0 }}
-          >
-            <Feedbackitem key={item.id} item={item} />
-          </motion.div>
-        ))}
-      </AnimatePresence>
-    </div>
-  );
+  // <div className="feedback-list">
+  //   {feedback.map((item) => (
+  //     <Feedbackitem key={item.id} item={item} handleDelete={handleDelete} />
+  //   ))}
+  // </div>
+  if (isLoading) {
+    return <Spinner />;
+  } else {
+    return (
+      <div className="feedback-list">
+        <AnimatePresence>
+          {feedback.map((item) => (
+            <motion.div
+              key={item.id}
+              initial={{ opacity: 0 }}
+              animate={{ opacity: 1 }}
+              exit={{ opacity: 0 }}
+            >
+              <Feedbackitem key={item.id} item={item} />
+            </motion.div>
+          ))}
+        </AnimatePresence>
+      </div>
+    );
+  }
 }
 
 export default FeedbackList;
